a0b4752203994b008e3d16f09a82dfe59da4722c
vincent <vincent@twroa.com>
2025-08-26 16:34:06 +0800

    fix cors

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -6,22 +6,24 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		origin := c.GetHeader("Origin") // 安全获取 Origin
-		if origin == "" {
-			origin = "*"
-		}
-		// 僅允許白名單 origin
-		if origin == "http://localhost:3000" || origin == "http://127.0.0.1:3000" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-
+		origin := c.GetHeader("Origin")
 		headers := c.Writer.Header()
-		headers.Set("Access-Control-Allow-Origin", origin)
-		headers.Set("Access-Control-Allow-Credentials", "true")
+		
+		// 允許所有來源
+		if origin != "" {
+			headers.Set("Access-Control-Allow-Origin", origin)
+			headers.Set("Access-Control-Allow-Credentials", "true")
+		} else {
+			headers.Set("Access-Control-Allow-Origin", "*")
+		}
+		
+		// 設置 CORS headers
 		headers.Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With, User-Agent, Pragma, Referer, X-Forwarded-For, X-Real-Ip, Accept-Language, utoken, x-key")
 		headers.Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, utoken, x-key")
 		headers.Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE, PATCH")
 		headers.Set("Referrer-Policy", "origin")
+		
+		// 處理 preflight 請求
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
